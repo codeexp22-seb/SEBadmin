f03e169366e192f0be1a491856bba3236395794a
Sean Wong <derpydm@gmail.com>
2022-06-14 20:25:55 +0800

    add redirect on successful login; fix issue with table heading

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Login.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons'
 export default function Home() {
@@ -13,8 +13,7 @@ export default function Home() {
       body: json
     })
     if (res.status === 200) {
-      // Logged in 
-      // Do something? idk
+      window.location.href = '/dashboard/home/'
       console.log("logged in successfully!")
     } else {
       console.log("Login failed.")
@@ -44,7 +43,7 @@ export default function Home() {
             <FontAwesomeIcon icon={faKey} />
               <input type="password" name="password" placeholder="Password" required/>
             </div>
-            <button onClick={loginUser}>Login</button>
+            <button>Login</button>
           </form>
         </div>
       </main>
