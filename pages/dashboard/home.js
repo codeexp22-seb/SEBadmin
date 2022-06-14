653965ba0dedecb1534bf5d0ad113fe4b47918c9
rapple.it <rachel.limre03@gmail.com>
2022-06-14 19:10:07 +0800

    added quick insights component in home page

--- pages/dashboard/home.js
+++ pages/dashboard/home.js
@@ -1,5 +1,6 @@
 import CourseList from "../../components/home/CourseList"
 import GroupList from "../../components/home/GroupList"
+import QuickInsights from "../../components/home/QuickInsights"
 import styles from '../../styles/home/Home.module.css'
 
 
@@ -8,8 +9,9 @@ const Home = () => {
         <main className={styles.main}>
             <div className={styles.content}>
                 <GroupList />
-                <div>
+                <div className={styles.secondRow}>
                     <CourseList />
+                    <QuickInsights />
                 </div>
             </div>
         </main>
